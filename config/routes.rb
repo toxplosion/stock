e39ead6372995ff3707c187bfc5698e48e69b148
Colin Herzog <colin.herzog@bluewin.ch>
2018-02-09 15:28:03 +0100

    added company

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'company#index'
+
   resources :companies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
